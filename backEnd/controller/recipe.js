c1a686b51cac7fa827af0afb4b85a16d9d35b160
Rutujeet28 <rutujeetfalmare28@gmail.com>
2025-09-24 16:59:27 -0500

    Database Connection and Created Schemas

--- backEnd/controller/recipe.js
+++ backEnd/controller/recipe.js
@@ -1,17 +1,43 @@
-const getRecipes = (req, res) => {            // defines a controller function getRecipes with the request, response objects
-    res.json({message : "hello"})
+const Recipes = require("../models/recipe")
+
+const getRecipes = async(req, res) => {            // defines a controller function getRecipes with the request, response objects
+    const recipes = await Recipes.find()
+    return res.json(recipes)
 }
 
-const getRecipe = (req, res) => {            // defines a controller function getRecipes with the request, response objects
-    res.json({message : "hello"})
+const getRecipe = async(req, res) => {            // defines a controller function getRecipes with the request, response objects
+    const recipe = await Recipes.findById(req.params.id)
+    res.json(recipe)
 }
 
-const addRecipe = (req, res) => {            // defines a controller function getRecipes with the request, response objects
-    res.json({message : "hello"})
+const addRecipe = async (req, res) => {            // defines a controller function getRecipes with the request, response objects
+    const {title, ingredients, instructions, time} = req.body
+
+    if(!title || !ingredients || !instructions){
+        res.json({message:"Required fields can't be empty."})
+    }
+
+    const newRecipe = await Recipes.create({
+        title, ingredients, instructions, time
+    })
+
+    return res.json(newRecipe)
 }
 
-const editRecipe = (req, res) => {            // defines a controller function getRecipes with the request, response objects
-    res.json({message : "hello"})
+const editRecipe = async(req, res) => {            // defines a controller function getRecipes with the request, response objects
+    const {title, ingredients, instructions, time} = req.body
+
+    let recipe = await Recipes.findById(req.params.id)
+
+    try{
+        if(recipe){
+            await Recipes.findByIdAndUpdate(req.params.id, req.params.body, {new : true})
+            res.json({title,ingredients, instructions, time})
+        }
+    }
+    catch(err){
+        return res.status(400).json({message : "error"})
+    }
 }
 
 const deleteRecipe = (req, res) => {            // defines a controller function getRecipes with the request, response objects
--- backEnd/server.js
+++ backEnd/server.js
@@ -6,6 +6,8 @@ const connectDb =  require("./config/connectionDb")
 const PORT = process.env.PORT || 3000  // access the port as defined in the env file else run it using 3000 as port num
 connectDb()
 
+app.use(express.json())
+
 app.use("/recipe", require("./routes/recipe"))
 
 app.listen(PORT,(err) => {                             // app.listen(PORT, callback) tells after server start listneing on the port and 2nd is callback func
